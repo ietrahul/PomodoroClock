1df1f9567b3afdb4b4d421798789ef19a65061b1
Rahul <ietrahul@gmail.com>
2017-07-02 19:46:47 +0530

    Session and break working CSS UI pending

--- pomodoro.js
+++ pomodoro.js
@@ -1,16 +1,27 @@
-var myVar, min, sec, totalTime, timeSpent=0;
+var myVar, min, sec, totalTime, timeSpent=0, i=0;
 
 function trial() {
 //myVar = setTimeout(myFunction, 1000);
- min = Number(document.getElementById("time").innerHTML);
+if (i== 0){
+ min = Number(document.getElementById("Sessiontime").innerHTML);
  sec = 0;
  totalTime = min*60;
  myVar = setInterval(myFunction, 1000);
 }
+ //call break time
+ if (i==1){
+ min = Number(document.getElementById("BreakTime").innerHTML);
+ sec = 0;
+ totalTime = min*60;
+ timeSpent=0;
+ document.getElementById("time").innerHTML = min;
+ myVar = setInterval(myFunction, 1000);
+ }
+}
 
 
 function myFunction() {
-  var x = document.getElementById("time").innerHTML;
+  //var x = document.getElementById("time").innerHTML;
   if (min > 0 ) {
     if (sec == 0){
       min -= 1;
@@ -22,30 +33,35 @@ function myFunction() {
     sec -= 1;
   }
     document.getElementById("time").innerHTML = (min > 9 ? min : "0"+min) +" : "+ (sec > 9 ? sec : "0"+sec) ;
-  if (min == 0 && sec == 0){
-    clearInterval(myVar);
-  }
   timeSpent +=1;
   var percentage = Math.floor((100*timeSpent/totalTime));
   if (percentage%2 == 0)  fill(percentage);
-}
-
-function add(){
-  clearInterval(myVar);
-  var x = document.getElementById("Sessiontime").innerHTML;
-  document.getElementById("Sessiontime").innerHTML = Number(x) + 1;
-  //document.getElementById("time").innerHTML = (Number(x) + 1) +" : " + "00";
-  document.getElementById("time").innerHTML = Number(x) + 1;
-}
-
-function subtract(){
-  clearInterval(myVar);
-  var x = document.getElementById("Sessiontime").innerHTML;
-   if (x != "1"){
-  document.getElementById("Sessiontime").innerHTML = Number(x) - 1;
-  //document.getElementById("time").innerHTML = (Number(x) - 1) +" : " + "00";
-  document.getElementById("time").innerHTML = Number(x) - 1;
+    if (min == 0 && sec == 0){
+    clearInterval(myVar);
+    i +=1;
+    if (i==1) trial();
+ console.log(i);
   }
+}
+
+function add(str){
+  clearInterval(myVar);
+  var x = document.getElementById(str).innerHTML;
+  document.getElementById(str).innerHTML = Number(x) + 1;
+  if (str === "Sessiontime") document.getElementById("time").innerHTML = Number(x) + 1;
+  i = 0;
+  fil(0);
+}
+
+function subtract(str){
+  clearInterval(myVar);
+  var x = document.getElementById(str).innerHTML;
+   if (x != "1"){
+  document.getElementById(str).innerHTML = Number(x) - 1;
+   if (str === "Sessiontime") document.getElementById("time").innerHTML = Number(x) - 1;
+  }
+  i = 0;
+  fil(0);
 }
 
 function fill(percentage){
